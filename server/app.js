5448d3c20b43a231650853583c4a898b3f953b7b
Baian Ou <baianou@Baians-MacBook-Pro.local>
2024-11-07 14:00:43 -0500

    enhanced server-side docs

--- server/app.js
+++ server/app.js
@@ -14,11 +14,10 @@ app.set('view engine', 'ejs');
 // Set the views directory (optional if your EJS files are in a folder named 'views')
 app.set('views', path.join(__dirname, 'views'));
 
-// Define a route
+// Define Homepage as API Docs
 app.get('/', (req, res) => {
    // Allow the docs.html template to 'include' markdown files
    // Solution from https://stackoverflow.com/questions/16369987/how-can-i-render-markdown-with-ejs-using-express-node-js
-   
    var md = function (filename) {
         var filePath = path.join(__dirname, '../Design Docs/API.md');
         var include = fs.readFileSync (filePath, 'utf8');
@@ -29,6 +28,33 @@ app.get('/', (req, res) => {
   
      res.render ('docs', {"md": md});
 });
+
+// Docs about how to start the server
+app.get('/server', (req, res) => {
+ var md = function (filename) {
+         var filePath = path.join(__dirname, '../HowToStart.md');
+         var include = fs.readFileSync (filePath, 'utf8');
+         var html = marked(include);
+   
+         return html;
+      };
+   
+      res.render ('docs', {"md": md});
+ });
+
+
+// README.md
+ app.get('/server/README.md', (req, res) => {
+    var md = function (filename) {
+         var filePath = path.join(__dirname, '../README.md');
+         var include = fs.readFileSync (filePath, 'utf8');
+         var html = marked(include);
+   
+         return html;
+      };
+   
+      res.render ('docs', {"md": md});
+ });
 
 // Use mixRoutes
 app.use('/api/mixes', mixRoutes);
