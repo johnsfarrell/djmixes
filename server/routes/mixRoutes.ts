7633bb3abc846a4e63aaf9b2e475f92c9d196f6e
Baian Ou <baianou@Baians-MBP.cable.rcn.com>
2024-11-18 14:32:29 -0500

    revert back the mock

--- server/routes/mixRoutes.ts
+++ server/routes/mixRoutes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 const mixController = new MixController();
 
 // Route for fetching a mix by its ID
-router.get('/:mixId', mixController.getMix);
+router.get('/:mixId', mixController.getMixMock);
 
 // Route for uploading a mix
 router.get('/:mixId/download', mixController.downloadMix);
