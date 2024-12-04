2f6976417702f73bf237783bf19dcc52f2cb4a94
John Farrell <jsfarrell12@gmail.com>
2024-12-04 11:26:55 -0500

    chore: update testing shell script

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,3 +5,11 @@ CURRENT_DIR=$(pwd)
 cd algorithm || exit
 python -m unittest discover -s tests -p "*_test.py"
 cd "$CURRENT_DIR" || exit
+
+cd server || exit
+npm test
+cd "$CURRENT_DIR" || exit
+
+cd server/dist/database/db_test || exit
+node db_test.js
+cd "$CURRENT_DIR" || exit
