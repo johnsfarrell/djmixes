5db6898a084ea73c10210b8af09d692bc5c28b11
Anita1014 <alice10zjy@163.com>
2024-12-10 21:10:10 -0500

    correct getFollows

--- server/src/database/search/getFollows.ts
+++ server/src/database/search/getFollows.ts
@@ -2,9 +2,9 @@ import createConnection from "../connection";
 import { FieldPacket, RowDataPacket } from "mysql2";
 
 /**
- * Function to get all artists followed by a user
+ * Function to get all users (acting as artists) followed by a user
  * @param userId - The ID of the user
- * @returns Promise<RowDataPacket[]> - List of followed artists
+ * @returns Promise<RowDataPacket[]> - List of followed users
  * @throws Error if the query fails
  */
 export const getFollowedArtists = async (
@@ -14,16 +14,16 @@ export const getFollowedArtists = async (
   try {
     const [rows]: [RowDataPacket[], FieldPacket[]] = await connection.execute(
       `
-      SELECT a.artist_id as artistId, a.name, a.profile_url as profileUrl, a.avatar
+      SELECT u.user_id as artistId, u.username as name, u.email as profileUrl, u.create_time as avatar
       FROM follows f
-      JOIN artists a ON f.artist_id = a.artist_id
+      JOIN users u ON f.artist_id = u.user_id
       WHERE f.user_id = ?
       `,
       [userId],
     );
     return rows;
   } catch (error) {
-    console.error("Error fetching followed artists:", error);
+    console.error("Error fetching followed users:", error);
     throw new Error("Database error occurred");
   } finally {
     await connection.end();
